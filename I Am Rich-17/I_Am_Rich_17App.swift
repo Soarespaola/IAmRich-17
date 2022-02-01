8e7d575c3913c5203fbf45bc8829186a26307f29
Paola Soares <paolaalcantara1@yahoo.com.br>
2022-02-01 00:21:23 -0300

    Projeto em SwiftUI

---
+++ I Am Rich-17/I_Am_Rich_17App.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_17App.swift
+//  I Am Rich-17
+//
+//  Created by Paola Alcantara Soares on 31/01/22.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
